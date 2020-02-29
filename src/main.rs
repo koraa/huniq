e44385878c62759a9d47fb40ee667e0bde5067a9
louy2 <2263580+louy2@users.noreply.github.com>
2020-02-29 17:41:20 -0500

    rustfmt main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,15 @@
 mod xxhash;
 mod xxhash_bindings;
 
-use std::collections::{HashSet, HashMap, hash_map};
-use std::hash::{Hasher, BuildHasher};
-use std::io::{stdin, Read, BufRead, BufReader, stdout, Write, BufWriter, ErrorKind};
-use std::{slice, default::Default, marker::PhantomData};
-use sysconf::page::pagesize;
-use anyhow::Result;
-use clap::{Arg, App};
-use getrandom::getrandom;
 use crate::xxhash::xxh3_u64_secret;
-
+use anyhow::Result;
+use clap::{App, Arg};
+use getrandom::getrandom;
+use std::collections::{hash_map, HashMap, HashSet};
+use std::hash::{BuildHasher, Hasher};
+use std::io::{stdin, stdout, BufRead, BufReader, BufWriter, ErrorKind, Read, Write};
+use std::{default::Default, marker::PhantomData, slice};
+use sysconf::page::pagesize;
 
 /// A no-operation hasher. Used as part of the uniq implementation,
 /// because in there we manually hash the data and just store the
@@ -24,7 +23,8 @@ struct IdentityHasher {
 impl Hasher for IdentityHasher {
     fn write(&mut self, bytes: &[u8]) {
         self.off += (&mut self.buf[self.off as usize..])
-            .write(bytes).unwrap_or(0) as u8;
+            .write(bytes)
+            .unwrap_or(0) as u8;
     }
 
     fn finish(&self) -> u64 {
@@ -58,11 +58,11 @@ impl<H: Hasher + Default> BuildHasher for BuildDefaultHasher<H> {
 /// The tokens found will be passed to the given callback. The tokens will
 /// always be terminated by the delimiter at the end. Even if the last token
 /// is delimited by eof and not the delimiter, a delimiter will be supplied.
-fn split_read_zerocopy<R, F>(
-        delim: u8, inp: &mut R, mut handle_line: F)
-        -> Result<()>
-        where R: Read, F: FnMut(&[u8]) -> Result<()> {
-
+fn split_read_zerocopy<R, F>(delim: u8, inp: &mut R, mut handle_line: F) -> Result<()>
+where
+    R: Read,
+    F: FnMut(&[u8]) -> Result<()>,
+{
     // Initialize the buffer
     let mut buf = vec![0u8; pagesize() * 2]; // buf.capacity() == buf.len() == pagesize() * 2
 
@@ -73,10 +73,11 @@ fn split_read_zerocopy<R, F>(
     loop {
         match inp.read(&mut buf[used..] /* unused space */) {
             Err(ref e) if e.kind() == ErrorKind::Interrupted =>
-                // EINTR – thrown when a process receives a signal…
-                continue,
-            Err(e) =>
-                return Err(anyhow::Error::from(e)),
+            // EINTR – thrown when a process receives a signal…
+            {
+                continue
+            }
+            Err(e) => return Err(anyhow::Error::from(e)),
             Ok(0) => {
                 // EOF; we potentially need to process the last word here
                 // if the input is missing a newline (or rather delim) at
@@ -94,16 +95,19 @@ fn split_read_zerocopy<R, F>(
                 }
 
                 break;
-            }, Ok(len) =>
-                // Register the data that became available
+            }
+            Ok(len) =>
+            // Register the data that became available
+            {
                 used += len
+            }
         };
 
         // Scan the buffer for lines
         let mut line_start: usize = 0;
         for (off, chr) in (&buf[..used]).iter().enumerate() {
             if *chr == delim {
-                handle_line(&buf[line_start..off+1])?;
+                handle_line(&buf[line_start..off + 1])?;
                 line_start = off + 1;
             }
         }
@@ -130,8 +134,12 @@ fn count_cmd(delim: u8) -> Result<()> {
     let mut set = HashMap::<Vec<u8>, u64>::new();
     for line in BufReader::new(stdin().lock()).split(delim) {
         match set.entry(line?) {
-            hash_map::Entry::Occupied(mut e) => { *e.get_mut() += 1; },
-            hash_map::Entry::Vacant(e)   => { e.insert(1); }
+            hash_map::Entry::Occupied(mut e) => {
+                *e.get_mut() += 1;
+            }
+            hash_map::Entry::Vacant(e) => {
+                e.insert(1);
+            }
         }
     }
 
@@ -159,7 +167,7 @@ fn uniq_cmd(delim: u8) -> Result<()> {
     getrandom(&mut secret)?;
 
     split_read_zerocopy(delim, &mut inp.lock(), |line| {
-        let tok: &[u8] = &line[..line.len()-1];
+        let tok: &[u8] = &line[..line.len() - 1];
         if set.insert(xxh3_u64_secret(&tok, &secret)) {
             out.write(&line)?;
         }
@@ -169,49 +177,55 @@ fn uniq_cmd(delim: u8) -> Result<()> {
     Ok(())
 }
 
-
 fn try_main() -> Result<()> {
     let mut argspec = App::new("huniq")
         .version("2.1.0")
         .about("Remove duplicates from stdin, using a hash table")
         .author("Karolin Varner <karo@cupdev.net)")
-        .arg(Arg::with_name("count")
-            .help("Output the amount of times a line was encountered")
-            .long("count")
-            .short("c"))
-        .arg(Arg::with_name("delimiter")
-            .help("Which delimiter between elements to use. By default `\n` is used")
-            .long("delimiter")
-            .long("delim")
-            .short("d")
-            .takes_value(true)
-            .default_value("\n")
-            .validator(|v| match v.len() {
-                1 => Ok(()),
-                _ => Err(String::from("\
+        .arg(
+            Arg::with_name("count")
+                .help("Output the amount of times a line was encountered")
+                .long("count")
+                .short("c"),
+        )
+        .arg(
+            Arg::with_name("delimiter")
+                .help("Which delimiter between elements to use. By default `\n` is used")
+                .long("delimiter")
+                .long("delim")
+                .short("d")
+                .takes_value(true)
+                .default_value("\n")
+                .validator(|v| match v.len() {
+                    1 => Ok(()),
+                    _ => Err(String::from(
+                        "\
 Only ascii characters are supported as delimiters. \
 Use sed to turn your delimiter into zero bytes?
 
     $ echo -n \"1λ1λ2λ3\" | sed 's@λ@\x00@g' | huniq -0 | sed 's@\x00@λ@g'
-    1λ2λ3λ"
-                ))
-            }))
-        .arg(Arg::with_name("null")
-            .help("Use the \\0 character as the record delimiter.")
-            .long("null")
-            .short("0")
-            .conflicts_with("delimiter"));
+    1λ2λ3λ",
+                    )),
+                }),
+        )
+        .arg(
+            Arg::with_name("null")
+                .help("Use the \\0 character as the record delimiter.")
+                .long("null")
+                .short("0")
+                .conflicts_with("delimiter"),
+        );
 
     let args = argspec.get_matches_from_safe_borrow(&mut std::env::args_os())?;
 
     let delim = match args.is_present("null") {
-            true  => b'\0',
-            false => args.value_of("delimiter").unwrap().as_bytes()[0]
+        true => b'\0',
+        false => args.value_of("delimiter").unwrap().as_bytes()[0],
     };
 
     match args.is_present("count") {
-        true  => count_cmd(delim),
-        false => uniq_cmd(delim)
+        true => count_cmd(delim),
+        false => uniq_cmd(delim),
     }
 }
 
